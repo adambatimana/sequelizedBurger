d4f486932fa893231b0192e5f22d44b5ec69ae86
Adam Batimana <adambatimana@gmail.com>
2017-08-08 18:33:15 -0400

    updated seq

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,68 +1,83 @@
 let express = require("express");
-// let burger = require("../models/burger.js");
 let path = require("path");
 const app = express();
-// const port = 8889;
+let db = require("../models/burger.js");
 
 module.exports = function(app) {
 
-  //============================================
-  //=============== API ROUTES  ================
-  //============================================
+    //============================================
+    //=============== API ROUTES  ================
+    //============================================
 
-  //============================
-  //======= GET REQUEST ========
-  //============================
+    //============================
+    //======= GET REQUEST ========
+    //============================
 
-  app.get("/", function(req, res) {
-          res.redirect("/burgers")
-  })
+    app.get("/", function(req, res) {
+        res.redirect("/burgers")
+    })
 
-  app.get("/burgers", function(req, res) {
-        console.log("Hit route");
-        //funciton to select all from burgers table
-        .all(function(data) {
-          console.log("Console log for data")
-          console.log(data)
-          //res.json(data);
-          //then send to HANDLEBARS
-          res.render("index", {
-            burgerToDevour: data
-          })
-        })
-  });
+    app.get("/burgers", function(req, res) {
+        console.log(db.Burgers)
+            // .findAll({}).then(function(dbBurger) {
+            //     // We have access to the todos as an argument inside of the callback function
+            //     res.json(dbBurger);
+            // });
 
-  //============================
-  //======= POST REQUEST =======
-  //============================
-  app.post("/burgers/new", function(req, res) {
-    console.log(req.body);
-    //select name, if devoured and date from AJAX call from HTML and create for database
-        .create(req.params.burger_name, function(result){
-            console.log(result);
+        //     //then send to HANDLEBARS
+        //     res.render("index", {
+        //         burgerToDevour: burgerData
+        //     })
+        // })
+    });
+
+    //============================
+    //======= POST REQUEST =======
+    //============================
+    app.post("/burgers/new", function(req, res) {
+
+        console.log(req.body);
+        // create takes an argument of an object describing the item we want to
+        // insert into our table. In this case we just we pass in an object with a text
+        // and complete property (req.body)
+        db.create({
+            burger_name: req.body.burger_name,
+            devoured: req.body.devoured,
+            date: req.body.date
+        }).then(function(dbburger) {
+            // We have access to the new todo as an argument inside of the callback function
+            res.json(dbburger);
+        });
+        //select name, if devoured and date from AJAX call from HTML and create for database
+        db.create({
+            burger_name: req.body.burger_name,
+            devoured: req.body.devoured,
+            date: req.body.date
+        }).then(function(burgerTodo) {
+            console.log(burgerTodo);
             res.redirect("/");
         });
-  });
+    });
 
 
-  //============================
-  //======= PUT REQUEST =======
-  //============================
-  app.put("/burgers/update", function(req,res){
-      .update(req.body.burger_id, function(result){
-        console.log(result);
-        res.redirect("/");
-      })
-  });
+    //============================
+    //======= PUT REQUEST =======
+    //============================
+    app.put("/burgers/update", function(req, res) {
+        db.update({}).then(function(burgerUpdate) {
+            console.log(burgerUpdate);
+            res.redirect("/");
+        })
+    });
 
-  //============================================
-  //=============== TEST ROUTE  ================
-  //============================================
-  // app.get("/", function(req, res) {
-  //   //test for HTML CONNECTION
-  //   res.sendFile(path.join(__dirname, "../public/test.html"));
-  //   //SHOULD HAVE HANDLEBARS HTML INSERTED HERE
-  // });
+    //============================================
+    //=============== TEST ROUTE  ================
+    //============================================
+    // app.get("/", function(req, res) {
+    //   //test for HTML CONNECTION
+    //   res.sendFile(path.join(__dirname, "../public/test.html"));
+    //   //SHOULD HAVE HANDLEBARS HTML INSERTED HERE
+    // });
 
 
 };
--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,8 @@
-module.exports = function(sequelize, DataTypes){
-    let burgers = sequelize.define("Todo", {
+module.exports = function(sequelize, DataTypes) {
+    let burgers = sequelize.define("Burgers", {
         burger_name: DataTypes.STRING,
         devoured: DataTypes.BOOLEAN,
         date: DataTypes.CURRENT_TIMESTAMP
     });
-      return Todo
+    return Todo
 };
